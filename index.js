8c7ebc3174eb1fe3a49d09dbe29828b2e3b4c063
Carl <carljeanpierre@gmail.com>
2023-07-16 20:50:25 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+let currentUser = 'Carl';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+let firstInitial = currentUser[0];
+let shortGreeting = 'Welcome, ' + firstInitial + '!';
+
